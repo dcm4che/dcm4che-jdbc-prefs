b192832aaf5715fb36984592945b98b28696035e
romank-agfa <roman.khazankin@agfa.com>
2014-07-15 15:57:11 +0200

    jdbcprefs2xml tool (fully delegates to prefs2xml)

--- dcm4che-jdbc-prefs-tool/src/bin/jdbcprefs2xml.bat
+++ dcm4che-jdbc-prefs-tool/src/bin/jdbcprefs2xml.bat
@@ -4,4 +4,4 @@ rem jdbcprefs2xml Launcher
 rem -------------------------------------------------------------------------
 set MAIN_CLASS=org.dcm4che.jdbc.prefs.xmlprefs2jdbc.JdbcPrefs2Xml
 			
-xmlprefs2jdbc.bat %* 
+%~dp0/xmlprefs2jdbc.bat %* 
